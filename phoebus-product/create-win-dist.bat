5821a953e42564b98c2c9bc21f35a3f4c18f75ed
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2024-09-05 12:28:12 +0200

    Fixed batch file for creating windows phoebus zip file used by the update.

--- phoebus-product/create-win-dist.bat
+++ phoebus-product/create-win-dist.bat
@@ -1,11 +1,20 @@
 @echo off
-REM Build  phoebus product for Windows
+REM Build  phoebus product for Windows.
+REM Run this batch script from the top directory: phoebus_product\create-win-dist.bat
 REM Create phoebus-win.zip package - used docs created in linux package.
+REM Requires command line tool to make a zip archive because the file produced by 
+REM PowerShell command Compress-Archive is not correctly unpacked by the CSS update module.
 
 set "VERSION=4.7.4-SNAPSHOT"
 
 @cd %~P0
 setlocal ENABLEDELAYEDEXPANSION
+
+rem echo %~P0
+if exist "%~P0phoebus-win.zip" (
+    del /q "%~P0phoebus-win.zip"
+    echo Deleted the old "%~P0phoebus-win.zip"
+)
 
 rem Get today's date in YYYY-MM-DD format
 for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (
@@ -51,12 +60,18 @@ if %ERRORLEVEL% NEQ 0 (
 )
 
 rem Extract the doc folder from phoebus-linux.zip
-echo Extracting the data from phoebus-linux.zip to copy doc directory and settings_template.ini
-rd /s /q temp_extracted
-powershell Expand-Archive -Path "phoebus-product\phoebus-linux.zip" -DestinationPath "temp_extracted" -Force
 
 set "ZIPFOLDER=temp_extracted"
 set "NEWZIP=phoebus-%VERSION%"
+rd /s /q %ZIPFOLDER%
+set "zip_cmd=C:\Program Files\7-Zip\7z.exe"
+if exist "%zip_cmd%" (
+    echo Extracting the data from phoebus-linux.zip to copy doc directory and settings_template.ini
+    "%zip_cmd%" x -tzip "phoebus-product\phoebus-linux.zip" -o"%ZIPFOLDER%"
+)
+
+rem powershell Expand-Archive -Path "phoebus-product\phoebus-linux.zip" -DestinationPath "temp_extracted" -Force
+
 mkdir %NEWZIP%\lib
 
 echo Copying all needed files to %NEWZIP%
@@ -69,16 +84,25 @@ copy phoebus-product\phoebus.* %NEWZIP%
 copy phoebus-product\settings.* %NEWZIP%
 copy phoebus-product\target\product-*jar %NEWZIP%
 
-rd /s /q temp_extracted
+echo removing %ZIPFOLDER%
+rd /s /q %ZIPFOLDER%
+
+if not exist "%zip_cmd%" (
+ 	echo 7-zip program not installed. Install it and run the script again
+ 	echo or send .\phoebus-%VERSION% to commpressed folder and rename it to phoebus-win.zip
+	pause
+ 	exit /b
+rem Compress-Archive creates the archive that can't be properly read by Control System Studio
+rem powershell Compress-Archive -Path ".\phoebus-%VERSION%" -DestinationPath ".\phoebus-product\phoebus-win.zip" -Force
+)
+
 rem Create new phoebus-win.zip archive
+echo compressing ".\phoebus-%VERSION%" into ".\phoebus-product\phoebus-win.zip" 
+"%zip_cmd%" a -tzip ".\phoebus-product\phoebus-win.zip" ".\phoebus-%VERSION%"
 
-powershell Compress-Archive -Path "phoebus-%VERSION%\*" -DestinationPath ".\phoebus-product\phoebus-win.zip" -Force
-rem Clean up temporary files
-echo Clean up temporary files
-rd /s /q temp_extracted
+echo removing %NEWZIP% directory
 rd /s /q %NEWZIP%
-rd /s /q phoebus-product\tmp-zip
-
 echo All commands executed successfully
 echo A new product package phoebus-win.zip has been created
+
 
